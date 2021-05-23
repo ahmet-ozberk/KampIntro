9cb649188fbffb5b50e1043180cfe44b60d6c147
Ahmet Özberk <ahmet.ozberk.mrs@gmail.com>
2021-05-23 20:04:44 +0300

    C# Kamp

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(string ad, string soyad,int musteriNo) {
+            Musteri musteri = new Musteri();
+            musteri.Ad = ad;
+            musteri.Soyad = soyad;
+            musteri.MusteriNo = musteriNo;
+            Console.WriteLine("\n\n" + ad +" "+soyad+" müşterisi eklendi.");
+        }
+
+        public void MusteriListele(Musteri[] musteri) {
+            Console.WriteLine("\n\nMüşteri Listesi\n");
+            foreach (Musteri i in musteri)
+            {
+                Console.WriteLine("Müşteri No: "+i.MusteriNo +"\nMüşteri Ad:" +i.Ad +"\nMüşteri Soyad:"+ i.Soyad);
+                Console.WriteLine("________________________");
+            }
+        }
+
+        public void MusteriSil(Musteri musteri) 
+        {
+            Console.WriteLine("\n\n"+musteri.MusteriNo+" müşteri numaralı "+musteri.Ad+" başarıyla silindi.");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MusteriManager musteriManager = new MusteriManager();
+            Musteri m1 = new Musteri();
+            m1.Ad = "Ahmet";
+            m1.Soyad = "Özberk";
+            m1.MusteriNo = 2757;
+
+            Musteri m2 = new Musteri();
+            m2.Ad = "Zeynep";
+            m2.Soyad = "Kır";
+            m2.MusteriNo = 814;
+
+            Musteri m3 = new Musteri();
+            m3.Ad = "Jhonny";
+            m3.Soyad = "S";
+            m3.MusteriNo = 3131;
+            Musteri[] musteriler = {m1,m2,m3 };
+           
+            musteriManager.MusteriEkle("Salih","Gültekin",657);
+            musteriManager.MusteriListele(musteriler);
+            musteriManager.MusteriSil(m3);
+
+        }
+    }
+}
